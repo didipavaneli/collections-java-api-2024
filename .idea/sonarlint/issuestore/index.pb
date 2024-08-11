7df0e263ff4ad8c82e5226b766402bb67b7150c2
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-08-11 18:45:03 -0300

    list pesquisa

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,9 @@ _
 /src/main/list/operacoesBasicas/ListaTarefa.java,3\f\3f26e3130a40d8c0ae55d0c35fff4329cfffcdd1
 Z
 *src/main/list/operacoesBasicas/Tarefa.java,4\d\4dd00991acd7ec4a11c7b628f518d25b7c823618
+Q
+!src/main/list/pesquisa/Livro.java,d\4\d4b48b7b192db25fc74ede5fca1fbb6ddf2ed962
+Z
+*src/main/list/pesquisa/CatalogoLivros.java,6\f\6f853cf3762bed6da44133d85107bd9fa8a4f361
+W
+'src/main/list/pesquisa/SomaNumeros.java,d\0\d07196659d35ae0c5e330645d070886bd70a99b2
---
+++ src/main/list/pesquisa/Livro.java
@@ -0,0 +1,39 @@
+package main.list.pesquisa;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
